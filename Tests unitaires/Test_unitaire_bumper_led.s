5757d34176b26a45fe22aa377ac54c1430c1fa1c
Valentinhdn <136761071+Valentinhdn@users.noreply.github.com>
2026-01-13 22:20:20 +0100

    Update and rename Test_unitaire_bumper_led.s to Tests unitaires/Test_unitaire_bumper_led.s

--- Tests unitaires/Test_unitaire_bumper_led.s
+++ Tests unitaires/Test_unitaire_bumper_led.s
@@ -1,5 +1,5 @@
 	;; RK - Evalbot (Cortex M3 de Texas Instrument)
-; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (Evalbot tourne sur lui même)
+; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (Evalbot tourne sur lui mÃªme)
 GPIO_BUMPER_L       EQU 0x01      ; Bumper Gauche broche 0 
 GPIO_BUMPER_R       EQU 0x02 	  ; Bumper Droit broche 1
 
