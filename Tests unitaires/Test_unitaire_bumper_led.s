e9877bb6e70e0193ca73709f793a1591a82688da
Valentinhdn <136761071+Valentinhdn@users.noreply.github.com>
2026-01-14 13:57:05 +0100

    Add files via upload

--- Tests unitaires/Test_unitaire_bumper_led.s
+++ Tests unitaires/Test_unitaire_bumper_led.s
@@ -1,5 +1,5 @@
 	;; RK - Evalbot (Cortex M3 de Texas Instrument)
-; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (Evalbot tourne sur lui mÃªme)
+	; programme - Test unitaire : Bumpers, Leds (on allume une allume selon le bumper appuyé)
 GPIO_BUMPER_L       EQU 0x01      ; Bumper Gauche broche 0 
 GPIO_BUMPER_R       EQU 0x02 	  ; Bumper Droit broche 1
 
@@ -8,7 +8,6 @@ GPIO_BUMPER_R       EQU 0x02 	  ; Bumper Droit broche 1
 		ENTRY
 		EXPORT	__main
 		
-		;; The IMPORT command specifies that a symbol is defined in a shared object at runtime.
 		IMPORT	BUMPER_init
 		IMPORT 	BUMPER_read				
 		IMPORT	LED_init
@@ -17,31 +16,43 @@ GPIO_BUMPER_R       EQU 0x02 	  ; Bumper Droit broche 1
 		IMPORT	LED2_ON
 		IMPORT	LED2_OFF
 		IMPORT	Clock_Enable
-		
+		IMPORT  MOTEUR_GAUCHE_OFF	
+		IMPORT  MOTEUR_DROIT_OFF	
+		IMPORT	MOTEUR_INIT	
 
 
 __main	
-
+		; initialisation de la config matérielle
 		BL Clock_Enable
 		BL LED_init
 		BL BUMPER_init
 
 loop
+		; lecture et test état du bumper Left
         BL BUMPER_read      
         TST R0, #GPIO_BUMPER_L
+		; si collision led1 allumée
         BEQ led1_on
 led1_off
+		; led1 éteinte
 		BL LED1_OFF
 		B check_bumperR
 led1_on
+		; led1 allumée
         BL LED1_ON
 		B check_bumperR
 check_bumperR
+		; test état bumper Right
         TST R0, #GPIO_BUMPER_R
+		; si collision led2 allumée
         BEQ led2_on
 led2_off
+		; led2 éteinte
 		BL LED2_OFF
+		; on reboucle à loop pour recommencer
 		B loop
 led2_on
+		; led2 allumée
         BL LED2_ON
+		; on reboucle à loop pour recommencer 
         B loop
--- Tests unitaires/Test_unitaire_switch_led.s
+++ Tests unitaires/Test_unitaire_switch_led.s
@@ -1,5 +1,5 @@
 	;; RK - Evalbot (Cortex M3 de Texas Instrument)
-; programme - Pilotage 2 Moteurs Evalbot par PWM tout en ASM (Evalbot tourne sur lui mÃªme)
+	; programme - Test unitaire : Switchs, Leds (on allume une allume selon le switch appuyé)
 GPIO_BUMPER_L       EQU 0x01      ; Bumper Gauche broche 0 
 GPIO_BUMPER_R       EQU 0x02 	  ; Bumper Droit broche 1
 GPIO_BP1			EQU 0x40	  ; bouton poussoir 1 broche 6
@@ -10,7 +10,6 @@ GPIO_BP2			EQU 0x80	  ; bouton poussoir 2 broche 7
 		ENTRY
 		EXPORT	__main
 		
-		;; The IMPORT command specifies that a symbol is defined in a shared object at runtime.
 		IMPORT	BUMPER_init
 		IMPORT 	BUMPER_read				
 		IMPORT	LED_init
@@ -24,26 +23,37 @@ GPIO_BP2			EQU 0x80	  ; bouton poussoir 2 broche 7
 
 
 __main
+	; initialisation de la config matérielle
     BL Clock_Enable
     BL LED_init
     BL SWITCH_init
 
 loop
+	; lecture et test état du switch1
     BL SWITCH_read
-    TST R1, #GPIO_BP1      ; si appuyÃ©
+    TST R1, #GPIO_BP1 
+	; si appuie, led1 allumée
     BEQ led1_on
 led1_off
+	; led1 éteinte
     BL LED1_OFF
     B check_bp2
 led1_on
+	; led1 allumée
     BL LED1_ON
     B  check_bp2
 check_bp2
+	; test état switch2
 	TST R1, #GPIO_BP2
+	; si appuie, led2 allumée
 	BEQ led2_on
 led2_off
+	; led2 éteinte
 	BL LED2_OFF
+	; on reboucle à loop pour recommencer
 	B  loop
 led2_on
+	; led2 allumée
 	BL LED2_ON
+	; on reboucle à loop pour recommencer
 	B loop
